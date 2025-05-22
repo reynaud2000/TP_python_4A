78363ae73f2a189ce8d1884205edab042975488b
Morgane <morgane@DESKTOP-G9200QR.lan>
2025-05-22 15:32:24 +0200

    test git

--- src/tp2/utils/analyze.py
+++ src/tp2/utils/analyze.py
@@ -2,32 +2,31 @@ from capstone import *
 from pylibemu import *
 import textwrap
 
-def disassemble(shellcode, address=0):
-    try:
-        md = Cs(CS_ARCH_X86, CS_MODE_32)
-        for i in md.disasm(shellcode, 0x1000):
-            print("0x{0:x}:\t{1}\t{2}".format(i.address, i.mnemonic, i.op_str))
-            if i.mnemonic == "jmp" and i.op_str == "short 0x54":
-                print("Found the jump instruction at address: 0x{0:x}".format(i.address))
-                break
-    except CsError as e:
-        print("ERROR: %s" % e)
-
 def analyzeShellcode(shellcode):
     hex_data = " ".join(f"{byte:02x}" for byte in shellcode)
     hex_str = textwrap.fill(hex_data, width=80)
+    print("Hexdump:")
     print(hex_str)
+    instructions = []
+    try:
+        from capstone import Cs, CS_ARCH_X86, CS_MODE_32
+        md = Cs(CS_ARCH_X86, CS_MODE_32)
+        for insn in md.disasm(shellcode, 0x1000):
+            instructions.append(f"0x{insn.address:08x}: {insn.mnemonic} {insn.op_str}")
+    except ImportError:
+        print("Capstone non installé, désassemblage non effectué.")
+        instructions.append("Désassemblage non disponible.")
+    emu_result = None
     try:
         from pylibemu import Emulator
         emu = Emulator()
         emu.prepare(shellcode, len(shellcode))
-        emu_result = emu.run(shellcode)
-        print("------ Emu résultat -------")
+        emu_result = emu.run()
+        print("\n------ Emulation Résultat -------")
         print(f"Résultat de l'émulateur : {emu_result}")
     except ImportError:
         print("pylibemu non installé, aucune émulation faîte.")
-        emu_result = None
-    instructions = []
+    
     info_all = (
         "Hexdump:\n" + hex_str + "\n\n"
         "Désassemblage:\n" + "\n".join(instructions) + "\n\n"
